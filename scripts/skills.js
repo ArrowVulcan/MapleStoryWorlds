29b86dcdb7e47f7704ec6620596a1ee18d967ef8
ArrowVulcan <stalkeer@hotmail.com>
2025-06-19 20:48:30 +0200

    Added Dark Knight and Skill Update
    Added the Dark Knight Job.
    Reworked the Class and Jobs skill menu.

---
+++ scripts/skills.js
@@ -0,0 +1,591 @@
+const Beginner = [
+    {
+        "name": "Recovery",
+        "koreanname": "회복",
+        "icon": "beginner/Skill_Recovery.png"
+    },
+    {
+        "name": "Nimble Feet",
+        "koreanname": "민첩한 몸놀림",
+        "icon": "beginner/Skill_Nimble_Feet.png"
+    },
+    {
+        "name": "Three Snails",
+        "koreanname": "달팽이 세마리",
+        "icon": "beginner/Skill_Three_Snails.png"
+    },
+    {
+        "name": "Blessing of the Fairy",
+        "koreanname": "정령의 축복",
+        "icon": "beginner/Skill_Blessing_Of_The_Fairy.png"
+    },
+    {
+        "name": "Empress's Blessing",
+        "koreanname": "여제의 축복",
+        "icon": "beginner/Skill_Empresss_Blessing.png"
+    },
+    {
+        "name": "Link Manager",
+        "koreanname": "링크 매니지먼트",
+        "icon": "beginner/Skill_Link_Manager.png"
+    }
+]
+
+const Warrior = [
+    {
+        "name": "Slash Blast",
+        "koreanname": "슬래시 블러스트",
+        "icon": "warrior/Skill_Slash_Blast.png"
+    },
+    {
+        "name": "War Leap",
+        "koreanname": "워리어 리프",
+        "icon": "warrior/Skill_War_Leap.png"
+    },
+    {
+        "name": "Upward Charge",
+        "koreanname": "어퍼 차지",
+        "icon": "warrior/Skill_Upward_Charge.png"
+    },
+    {
+        "name": "Leap Attack",
+        "koreanname": "리프 어택",
+        "icon": "warrior/Skill_Leap_Attack.png"
+    },
+    {
+        "name": "Iron Body",
+        "koreanname": "아이언 바디",
+        "icon": "warrior/Skill_Iron_Body.png"
+    },
+    {
+        "name": "Warrior Mastery",
+        "koreanname": "워리어 마스터리",
+        "icon": "warrior/Skill_Warrior_Mastery.png"
+    }
+]
+
+const job_skills = {
+
+    Hero: [
+
+        Beginner,
+        Warrior,
+        [
+            {
+                "name": "Brandish",
+                "koreanname": "브랜디쉬",
+                "icon": "fighter/Skill_Brandish.png"
+            },
+            {
+                "name": "Flash Blade",
+                "koreanname": "플래시 슬래시",
+                "icon": "fighter/Skill_Flash_Blade.png"
+            },
+            {
+                "name": "Combo Attack",
+                "koreanname": "콤보 어택",
+                "icon": "fighter/Skill_Combo_Attack.png"
+            },
+            {
+                "name": "Spirit Blade",
+                "koreanname": "스피릿 블레이드",
+                "icon": "fighter/Skill_Spirit_Blade.png"
+            },
+            {
+                "name": "Weapon Mastery",
+                "koreanname": "웨폰 마스터리",
+                "icon": "fighter/Skill_Weapon_Mastery.png"
+            },
+            {
+                "name": "Agile Arms",
+                "koreanname": "웨폰 액셀레이션",
+                "icon": "fighter/Skill_Agile_Arms.png"
+            },
+            {
+                "name": "Final Attack",
+                "koreanname": "파이널 어택",
+                "icon": "fighter/Skill_Final_Attack.png"
+            },
+            {
+                "name": "Physical Training",
+                "koreanname": "피지컬 트레이닝",
+                "icon": "fighter/Skill_Physical_Training.png"
+            },
+        ],
+        [
+            {
+                "name": "Intrepid Slash",
+                "koreanname": "브레이브 슬래시",
+                "icon": "crusader/Skill_Intrepid_Slash.png"
+            },
+            {
+                "name": "Rush",
+                "koreanname": "돌진",
+                "icon": "crusader/Skill_Rush.png"
+            },
+            {
+                "name": "Scarring Sword",
+                "koreanname": "스카링 소드",
+                "icon": "crusader/Skill_Scarring_Sword.png"
+            },
+            {
+                "name": "Combo Synergy",
+                "koreanname": "콤보 시너지",
+                "icon": "crusader/Skill_Combo_Synergy.png"
+            },
+            {
+                "name": "Self Recovery",
+                "koreanname": "셀프 리커버리",
+                "icon": "crusader/Skill_Self_Recovery.png"
+            },
+            {
+                "name": "Chance Attack",
+                "koreanname": "찬스 어택",
+                "icon": "crusader/Skill_Chance_Attack.png"
+            },
+            {
+                "name": "Endure",
+                "koreanname": "인듀어",
+                "icon": "crusader/Skill_Endure.png"
+            },
+            {
+                "name": "Beam Blade",
+                "koreanname": "오라 블레이드",
+                "icon": "crusader/Skill_Beam_Blade.png"
+            },
+        ],
+        [
+            {
+                "name": "Raging Blow",
+                "koreanname": "레이징 블로우",
+                "icon": "hero/Skill_Raging_Blow.png"
+            },
+            {
+                "name": "Puncture",
+                "koreanname": "인사이징",
+                "icon": "hero/Skill_Puncture.png"
+            },
+            {
+                "name": "Magic Crash",
+                "koreanname": "매직 크래쉬",
+                "icon": "hero/Skill_Magic_Crash.png"
+            },
+            {
+                "name": "Enrage",
+                "koreanname": "인레이지",
+                "icon": "hero/Skill_Enrage.png"
+            },
+            {
+                "name": "Maple Warrior",
+                "koreanname": "메이플 용사",
+                "icon": "hero/Skill_Maple_Warrior.png"
+            },
+            {
+                "name": "Hero's Will",
+                "koreanname": "용사의 의지",
+                "icon": "hero/Skill_Heros_Will.png"
+            },
+            {
+                "name": "Advanced Combo",
+                "koreanname": "어드밴스드 콤보",
+                "icon": "hero/Skill_Advanced_Combo.png"
+            },
+            {
+                "name": "Combat Mastery",
+                "koreanname": "컴뱃 마스터리",
+                "icon": "hero/Skill_Combat_Mastery.png"
+            },
+            {
+                "name": "Power Stance",
+                "koreanname": "스탠스",
+                "icon": "hero/Skill_Power_Stance.png"
+            },
+            {
+                "name": "Advanced Final Attack",
+                "koreanname": "어드밴스드 파이널 어택",
+                "icon": "hero/Skill_Advanced_Final_Attack.png"
+            },
+        ],
+        [
+            {
+                "name": "Decent Holy Symbol",
+                "koreanname": "쓸만한 홀리 심볼",
+                "icon": "hero/Skill_Decent_Holy_Symbol.png"
+            },
+            {
+                "name": "Sword Illusion",
+                "koreanname": "소드 일루전",
+                "icon": "hero/Skill_Sword_Illusion.png"
+            },
+            {
+                "name": "Instinctual Combo",
+                "koreanname": "콤보 인스팅트",
+                "icon": "hero/Skill_Instinctual_Combo.png"
+            },
+            {
+                "name": "Worldreaver",
+                "koreanname": "콤보 데스폴트",
+                "icon": "hero/Skill_Worldreaver.png"
+            },
+            {
+                "name": "Burning Soul Blade",
+                "koreanname": "소드 오브 버닝 소울",
+                "icon": "hero/Skill_Burning_Soul_Blade.png"
+            },
+        ]
+
+    ],
+
+    Paladin: [
+
+        Beginner,
+        Warrior,
+        [
+            {
+                "name": "Divine Swing",
+                "koreanname": "디바인 스윙",
+                "icon": "page/Skill_Divine_Swing.png"
+            },
+            {
+                "name": "Vessel of Light",
+                "koreanname": "홀리 차지",
+                "icon": "page/Skill_Vessel_Of_Light.png"
+            },
+            {
+                "name": "Close Combat",
+                "koreanname": "페이지 오더",
+                "icon": "page/Skill_Close_Combat.png"
+            },
+            {
+                "name": "Knockback Resistance",
+                "koreanname": "스탠스",
+                "icon": "page/Skill_Knockback_Resistance.png"
+            },
+            {
+                "name": "Weapon Mastery",
+                "koreanname": "웨폰 마스터리",
+                "icon": "page/Skill_Weapon_Mastery.png"
+            },
+            {
+                "name": "Final Attack",
+                "koreanname": "파이널 어택",
+                "icon": "page/Skill_Final_Attack.png"
+            },
+            {
+                "name": "Physical Training",
+                "koreanname": "피지컬 트레이닝",
+                "icon": "page/Skill_Physical_Training.png"
+            },
+        ],
+        [
+            {
+                "name": "Divine Charge",
+                "koreanname": "디바인 차지",
+                "icon": "white_knight/Skill_Divine_Charge.png"
+            },
+            {
+                "name": "HP Recovery",
+                "koreanname": "리스토네이션",
+                "icon": "white_knight/Skill_HP_Recovery.png"
+            },
+            {
+                "name": "Rush",
+                "koreanname": "돌진",
+                "icon": "white_knight/Skill_Rush.png"
+            },
+            {
+                "name": "Noble Demand",
+                "koreanname": "노블 디맨드",
+                "icon": "white_knight/Skill_Noble_Demand.png"
+            },
+            {
+                "name": "Parashock Guard",
+                "koreanname": "파라쇼크 가드",
+                "icon": "white_knight/Skill_Parashock_Guard.png"
+            },
+            {
+                "name": "Combat Orders",
+                "koreanname": "컴뱃 오더스",
+                "icon": "white_knight/Skill_Combat_Orders.png"
+            },
+            {
+                "name": "Shield Mastery",
+                "koreanname": "실드 마스터리",
+                "icon": "white_knight/Skill_Shield_Mastery.png"
+            },
+            {
+                "name": "Achilles",
+                "koreanname": "아킬레스",
+                "icon": "white_knight/Skill_Achilles.png"
+            },
+            {
+                "name": "Divine Shield",
+                "koreanname": "블레싱 아머",
+                "icon": "white_knight/Skill_Divine_shield.png"
+            },
+        ],[
+            {
+                "name": "Divine Mark",
+                "koreanname": "디바인 스티그마",
+                "icon": "paladin/Skill_Divine_Mark.png"
+            },
+            {
+                "name": "Blast",
+                "koreanname": "블래스트",
+                "icon": "paladin/Skill_Blast.png"
+            },
+            {
+                "name": "Divine Judgment",
+                "koreanname": "디바인 저지먼트",
+                "icon": "paladin/Skill_Divine_Judgment.png"
+            },
+            {
+                "name": "Magic Crash",
+                "koreanname": "매직 크래쉬",
+                "icon": "paladin/Skill_Magic_Crash.png"
+            },
+            {
+                "name": "Heaven's Hammer",
+                "koreanname": "생츄어리",
+                "icon": "paladin/Skill_Heavens_Hammer.png"
+            },
+            {
+                "name": "Divine Blessing",
+                "koreanname": "디바인 블레싱",
+                "icon": "paladin/Skill_Divine_Blessing.png"
+            },
+            {
+                "name": "Maple Warrior",
+                "koreanname": "메이플 용사",
+                "icon": "paladin/Skill_Maple_Warrior.png"
+            },
+            {
+                "name": "Hero's Will",
+                "koreanname": "용사의 의지",
+                "icon": "paladin/Skill_Heros_Will.png"
+            },
+            {
+                "name": "Guardian",
+                "koreanname": "가디언 스피릿",
+                "icon": "paladin/Skill_Guardian.png"
+            },
+            {
+                "name": "High Paladin",
+                "koreanname": "팔라딘 엑스퍼트",
+                "icon": "paladin/Skill_High_Paladin.png"
+            },
+            {
+                "name": "Greater Vessel of Light",
+                "koreanname": "어드밴스드 홀리 차지",
+                "icon": "paladin/Skill_Greater_Vessel_of_Light.png"
+            },
+        ],[
+            {
+                "name": "Divine Echo",
+                "koreanname": "홀리 유니티",
+                "icon": "paladin/Skill_Divine_Echo.png"
+            },
+            {
+                "name": "Mighty Mjolnir",
+                "koreanname": "마이티 묠니르",
+                "icon": "paladin/Skill_Mighty_Mjolnir.png"
+            },
+            {
+                "name": "Grand Guardian",
+                "koreanname": "그랜드 크로스",
+                "icon": "paladin/Skill_Grand_Guardian.png"
+            },
+            {
+                "name": "Hammers of the Righteous",
+                "koreanname": "블래스드 해머",
+                "icon": "paladin/Skill_Hammers_Of_The_Righteous.png"
+            },
+        ]
+
+    ],
+
+    Dark_Knight: [
+
+        Beginner,
+        Warrior,
+        [
+            {
+                "name": "Spear Sweep",
+                "koreanname": "스피어 풀링",
+                "icon": "spearman/Skill_Spear_Sweep.png"
+            },
+            {
+                "name": "Evil Eye",
+                "koreanname": "비홀더",
+                "icon": "spearman/Skill_Evil_Eye.png"
+            },
+            {
+                "name": "Evil Eye Shock",
+                "koreanname": "비홀더 쇼크",
+                "icon": "spearman/Skill_Evil_Eye_Shock.png"
+            },
+            {
+                "name": "Hyper Body",
+                "koreanname": "하이퍼 바디",
+                "icon": "spearman/Skill_Hyper_Body.png"
+            },
+            {
+                "name": "Iron Wall",
+                "koreanname": "아이언 월",
+                "icon": "spearman/Skill_Iron_Wall.png"
+            },
+            {
+                "name": "Weapon Mastery",
+                "koreanname": "웨폰 마스터리",
+                "icon": "spearman/Skill_Weapon_Mastery.png"
+            },
+            {
+                "name": "Agile Arms",
+                "koreanname": "웨폰 액셀레이션",
+                "icon": "spearman/Skill_Weapon_Mastery.png"
+            },
+            {
+                "name": "Final Attack",
+                "koreanname": "파이널 어택",
+                "icon": "spearman/Skill_Final_Attack.png"
+            },
+            {
+                "name": "Physical Training",
+                "koreanname": "피지컬 트레이닝",
+                "icon": "spearman/Skill_Physical_Training.png"
+            },
+        ],
+        [
+            {
+                "name": "La Mancha Spear",
+                "koreanname": "라만차 스피어",
+                "icon": "berserk/Skill_La_Mancha_Spear.png"
+            },
+            {
+                "name": "Rush",
+                "koreanname": "돌진",
+                "icon": "berserk/Skill_Rush.png"
+            },
+            {
+                "name": "Cross Surge",
+                "koreanname": "크로스 오버 체인",
+                "icon": "berserk/Skill_Cross_Surge.png"
+            },
+            {
+                "name": "Evil Eye of Domination",
+                "koreanname": "비홀더 도미넌트",
+                "icon": "berserk/Skill_Evil_Eye_Of_Domination.png"
+            },
+            {
+                "name": "Evil Eye Shock II",
+                "koreanname": "비홀더 쇼크 II",
+                "icon": "berserk/Skill_Evil_Eye_Shock_II.png"
+            },
+            {
+                "name": "Hex of the Evil Eye",
+                "koreanname": "비홀더스 버프",
+                "icon": "berserk/Skill_Hex_Of_The_Evil_Eye.png"
+            },
+            {
+                "name": "Lord of Darkness",
+                "koreanname": "로드 오브 다크니스",
+                "icon": "berserk/Skill_Lord_Of_Darkness.png"
+            },
+            {
+                "name": "Endure",
+                "koreanname": "인듀어",
+                "icon": "berserk/Skill_Endure.png"
+            },
+        ],
+        [
+            {
+                "name": "Gungnir's Descent",
+                "koreanname": "궁니르 디센트",
+                "icon": "dark_knight/Skill_Gungnirs_Descent.png"
+            },
+            {
+                "name": "Dark Impale",
+                "koreanname": "다크 임페일",
+                "icon": "dark_knight/Skill_Dark_Impale.png"
+            },
+            {
+                "name": "Accept Final Pact",
+                "koreanname": "리인카네이션 액셉트",
+                "icon": "dark_knight/Skill_Accept_Final_Pact.png"
+            },
+            {
+                "name": "Final Pact",
+                "koreanname": "리인카네이션",
+                "icon": "dark_knight/Skill_Final_Pact.png"
+            },
+            {
+                "name": "Dark Resonance",
+                "koreanname": "다크 레조넌스",
+                "icon": "dark_knight/Skill_Dark_Resonance.png"
+            },
+            {
+                "name": "Magic Crash",
+                "koreanname": "매직 크래쉬",
+                "icon": "dark_knight/Skill_Magic_Crash.png"
+            },
+            {
+                "name": "Maple Warrior",
+                "koreanname": "메이플 용사",
+                "icon": "dark_knight/Skill_Maple_Warrior.png"
+            },
+            {
+                "name": "Hero's Will",
+                "koreanname": "용사의 의지",
+                "icon": "dark_knight/Skill_Heros_Will.png"
+            },
+            {
+                "name": "Revenge of the Evil Eye",
+                "koreanname": "비홀더스 리벤지",
+                "icon": "dark_knight/Skill_Revenge_Of_The_Evil_Eye.png"
+            },
+            {
+                "name": "Barricade Mastery",
+                "koreanname": "어드밴스드 웨폰 마스터리",
+                "icon": "dark_knight/Skill_Barricade_Mastery.png"
+            },
+            {
+                "name": "Power Stance",
+                "koreanname": "스탠스",
+                "icon": "dark_knight/Skill_Power_Stance.png"
+            },
+        ],
+        [
+            {
+                "name": "Decent Holy Symbol",
+                "koreanname": "쓸만한 홀리 심볼",
+                "icon": "dark_knight/Skill_Decent_Holy_Symbol.png"
+            },
+            {
+                "name": "Spear of Darkness",
+                "koreanname": "다크 스피어",
+                "icon": "dark_knight/Skill_Spear_Of_Darkness.png"
+            },
+            {
+                "name": "Calamitous Cyclone",
+                "koreanname": "피어스 사이클론",
+                "icon": "dark_knight/Skill_Calamitous_Cyclone.png"
+            },
+            {
+                "name": "Darkness Aura",
+                "koreanname": "다크니스 오라",
+                "icon": "dark_knight/Skill_Darkness_Aura.png"
+            },
+            {
+                "name": "Radiant Evil",
+                "koreanname": "비홀더 임팩트",
+                "icon": "dark_knight/Skill_Radiant_Evil.png"
+            },
+        ]
+
+    ],
+
+    Dawn_Warrior: [
+        
+    ]
+
+}
